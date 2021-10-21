345fcd9fd3401307772eaa5e620778468a16c2a3
AlQalb <hayotbekyoldashev2@gmail.com>
2021-10-22 02:37:15 +0500

    Numbers Home Task

---
+++ JsNumbers/numbers.js
@@ -0,0 +1,152 @@
+// 1  piremetr
+var a = 4;
+var P = a * 4;
+console.log(P);
+
+// 2 Yuza
+
+var a = 3;
+var S = a ** 2;
+console.log(S);
+
+// 3  yuza P va S
+
+var a = 3;
+var b = 5;
+var S = a * b;
+var P = (a + b) * 2;
+console.log(S, P);
+
+// 4 diametr
+
+var d = 13;
+const p = 3.14;
+var L = p * d;
+console.log(L);
+
+// 5 kub yuza xajm
+
+var a = 5;
+var V = a ** 3;
+var S = 6 * a ** 2;
+console.log(S, V);
+
+// 6 paralelopepid
+
+var a = 2;
+var b = 3;
+var c = 4;
+var V = a * b * c;
+var S = 2 * (a * b + b * c + a * c);
+console.log(V, S);
+
+// 7 doira
+var R = 8;
+var L = 2 * p * R;
+var S = p * R ** 2;
+console.log(L, S);
+
+// 8 arifmetik
+var a = 5;
+var b = 3;
+var A = (a + b) / 2;
+console.log(A);
+
+// 9 ildiz osti
+
+var a = 4;
+var b = 9;
+var kub = (a * b) ** (1 / 2);
+console.log(kub);
+
+// 10
+
+var a = 10,
+  b = 12;
+var yigindi = a + b;
+var kopaytma = a * b;
+var kvadrat = a ** 2;
+let kvadratb = b ** 2;
+console.log(yigindi, kopaytma, kvadrat, kvadratb);
+
+// 11
+
+// 12
+
+var a = 12;
+var b = 13;
+var c = (a ** 2 + b ** 2) ** (1 / 2);
+var P = a + b + c;
+console.log(c, P);
+
+// 13
+
+var r1 = 10;
+var r2 = 5;
+var s1 = p * r1;
+var s2 = p * r2;
+var s3 = p * (r1 - r2);
+console.log(s1, s2, s3);
+
+// 14 aylananingradusi;
+var L = 12.56;
+var r = L / (2 * p);
+console.log(r, (s = p * r ** 2));
+
+// 15
+var S = 15;
+var R = (S / p) ** (1 / 2);
+console.log(R);
+
+// 16
+var x2 = 10;
+var x1 = 4;
+console.log(x2 - x1);
+
+// 17
+var A = 3;
+var B = 7;
+var C = 10;
+var AC = C - A;
+var BC = C - B;
+
+console.log(AC, BC, AC + BC);
+
+// 18
+var A = 2;
+var C = 8;
+var B = 13;
+var AC = C - A;
+var BC = B - C;
+console.log(AC, BC, AC * BC);
+
+// 20
+var x1 = 8;
+var y1 = 2;
+var x2 = 15;
+var y2 = 6;
+
+console.log(((x2 - x1) ** 2 + (y2 - y1) ** 2) ** (1 / 2));
+
+// 22
+
+var a = 34;
+var b = 42;
+var c;
+var c = a;
+var a = b;
+var b = c;
+
+console.log(a, b);
+
+// 23
+
+var a = 34;
+var b = 42;
+var c = 53;
+var d;
+var d = a;
+var a = b;
+var b = c;
+var c = d;
+console.log(a, b, c);
