268636f28c2487eddb7151d93a5753cc34f3c749
AlQalb <hayotbekyoldashev2@gmail.com>
2021-10-26 23:37:03 +0500

    jsString

---
+++ JsString/string.js
@@ -0,0 +1,18 @@
+var a = '555';
+var b = '2+2';
+
+console.log(typeof a);
+
+console.log(a.search('hello'));
+console.log(a.includes('js'));
+console.log(a.repeat(5));
+console.log(a.replace('js', 'Abdulaziz'));
+console.log(a.slice(0, 5));
+console.log(a.substring(0, 5));
+console.log(a.substr(0, 5));
+console.log(a.charAt());
+console.log(a.indexOf('js'));
+console.log(a.lastIndexOf('js'));
+console.log(eval(b));
+console.log(a.trim());
+console.log(a.padStart(5, '0'));
