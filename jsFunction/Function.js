8196a80a909803ce64a19c11177b31f2c77d087b
AlQalb <hayotbekyoldashev2@gmail.com>
2021-10-27 13:05:16 +0500

    JsFunction

--- jsFunction/Function.js
+++ jsFunction/Function.js
@@ -42,10 +42,39 @@ abs = (a) => {
   return (a ** 2) ** 0.5;
 };
 
-weC = 0;
-ac = 0;
-bc = 0;
-
 console.log(abs(8));
 
+let tekst = 'welcome webbrain'.repeat(5);
 
+var we = 0;
+var a = 0;
+var b = 0;
+
+sanoq = (sanoq) => {
+  index = tekst.indexOf('we');
+  we++;
+  tekst = tekst.slice(index + 2);
+  tekst.includes('we') && sanoq(sanoq);
+};
+sanoq(sanoq);
+
+let teksta = 'welcome webbrain'.repeat(5);
+
+sanoqa = (sanoq) => {
+  index = teksta.indexOf('a');
+  a++;
+  teksta = teksta.slice(index + 1);
+  teksta.includes('a') && sanoqa(sanoq);
+};
+sanoqa(sanoq);
+
+let tekstb = 'welcome webbrain'.repeat(5);
+sanoqb = (sanoq) => {
+  index = tekstb.indexOf('b');
+  b++;
+  tekstb = tekstb.slice(index + 1);
+  tekstb.includes('b') && sanoqb(sanoq);
+};
+sanoqb(sanoq);
+
+console.log(`we : ${we} ta`, `a : ${a} ta`, `b : ${b} ta`);
